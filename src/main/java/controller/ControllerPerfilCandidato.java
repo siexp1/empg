f19a109711d93100618f5a70b74958f0e92e27ba
Dener Carvalho <furiano.k@gmail.com>
2016-08-22 22:43:08 -0300

    Controller, Model, View
    Criação da classe PerfilCandidato corespondete aos pacotes Controller e
    Model, e criação da classe principal MainEmpreGO.

---
+++ src/main/java/controller/ControllerPerfilCandidato.java
@@ -0,0 +1,14 @@
+package controller;
+
+import model.PerfilCandidato;
+
+/**
+ * @author Dener
+ */
+public class ControllerPerfilCandidato {
+    PerfilCandidato perfilCandidato;
+    
+    public ControllerPerfilCandidato(PerfilCandidato perfilCandidato) { 
+        this.perfilCandidato = perfilCandidato;
+    }    
+}
